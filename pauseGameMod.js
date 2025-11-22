1c74a070a23102b5e8a10b57e5849c5ab3df148a
y <luw686021@gmail.com>
2025-11-21 22:06:03 -0800

    Add files via upload
    working version of pausingGameMod, but not finished

---
+++ pauseGameMod.js
@@ -0,0 +1,28 @@
+window.pauseGameMod = {};
+window.pauseGameMod.runCodeBefore = function() {
+  window.pauseGame = 0;
+}
+window.pauseGameMod.alterSnakeCode = function(code) {
+  //messes with the game frame by frame (update method)
+  code = assertReplace(
+    code,
+    /\(this\.Aa\.direction!=="NONE"\|\|CUD\(this\.Aa\)\)/,
+    "((this.Aa.direction!==\"NONE\"||CUD(this.Aa))&&!window.pauseGame)"
+  );
+  return code;
+}
+window.pauseGameMod.runCodeAfter = function() {
+  function keydownHandler(e){
+    if(e.code === "KeyQ"){
+      window.pauseGame = !window.pauseGame;
+      if(window.pauseGame){
+        document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.visibility = "visible";
+        document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.opacity = 1;
+      } else {
+        document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.visibility = "hidden";
+        document.querySelector("body > div.Czus3 > div > div.wjOYOd").style.opacity = 0;
+      }
+    }
+  }
+  document.addEventListener('keydown', keydownHandler);
+}
